a87412d0406751d45196d3aa044f456c5ce0d2bf
Lauren Dennedy <laurendennedy@gmail.com>
2024-04-18 01:27:14 -0400

    Reformat and comment Main.java
    Add comment block header to file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/**
+ * File: Main.java
+ * Authors: Lauren Dennedy G01462079, Lin Dai _________
+ * Class: CS 550 Database Systems
+ * Assignment: Group Project - Project 2
+ * Created: 4/10
+ * Last Modified: 4/18
+ */
+
 import java.sql.*; 
 import java.math.*; 
 import java.io.*;
@@ -8,46 +17,42 @@ import org.apache.commons.cli.*;
 
 class Main
 {
+    /*
+     * The main method represents the entry point for the program.
+     * The GUI view class is handled in Menu.java, and the jdbc
+     * driver is managed in Driver.java
+     */
     public static void main(String[] args) throws SQLException {
 
+        // Use a command line parser from Apache commons to accept
+        // a default file path as an argument to main
         Options options = new Options();
 
         Option input = new Option("i", "input", true, "input file path");
         input.setRequired(true);
         options.addOption(input);
 
-        /*
-        Option output = new Option("o", "output", true, "output file");
-        output.setRequired(true);
-        options.addOption(output);
-         */
-
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd = null; //not a good practice, it serves it purpose 
+        CommandLine cmd = null;
 
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
             formatter.printHelp("utility-name", options);
-
             System.exit(1);
         }
 
-        String inputFilePath = cmd.getOptionValue("input");
-        //String outputFilePath = cmd.getOptionValue("output");
-
-        System.out.println(inputFilePath);
-        //System.out.println(outputFilePath);
-
-        
+        String inputFilePath = (cmd != null) ? cmd.getOptionValue("input") : "";
+        System.out.println("Initializing Program with default file: " + inputFilePath);
 
         // Load main menu
         Menu.createAndShowGUI();
+
+        // Start the jdbc driver in the Driver class
         Driver.initDriver(inputFilePath);
 
-
-        //Driver.closeConnection();
+        // Driver is automatically closed in terminating states of the program
     }
 }
